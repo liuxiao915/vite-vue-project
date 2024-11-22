11d65f2187b64f85d4dca7c70b2db7554b900efc
liuxiao <1054105865@qq.com>
2024-11-22 17:59:03 +0800

    update: 表单构建

--- src/views/systemTools/Cesium/index.vue
+++ src/views/systemTools/Cesium/index.vue
@@ -3,7 +3,8 @@
 </template> 
   <script setup> 
 import * as Cesium from 'cesium';
-import { onMounted } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+const emit = defineEmits(['cesium'])
 const props = defineProps({
   options: {
     type: Object,
@@ -27,32 +28,34 @@ const props = defineProps({
     }
   }
 })
+const viewer = ref(null)
 onMounted(() => {
   // 设置默认视角在中国
   Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(90, -20, 110, 90);
-  const viewer = new Cesium.Viewer("cesiumContainer", { ...props.options });
+  viewer.value = new Cesium.Viewer("cesiumContainer", { ...props.options });
   // NOTE:修改鼠标操作习惯
   // 将原来鼠标中键倾斜视图修改为鼠标左键拖动
-  viewer.scene.screenSpaceCameraController.tiltEventTypes = [
+  viewer.value.scene.screenSpaceCameraController.tiltEventTypes = [
     Cesium.CameraEventType.RIGHT_DRAG,
   ];
   // 将原来鼠标右键拖动缩放修改为鼠标滚轮滚动
-  viewer.scene.screenSpaceCameraController.zoomEventTypes = [
+  viewer.value.scene.screenSpaceCameraController.zoomEventTypes = [
     Cesium.CameraEventType.WHEEL,
   ];
   // 开启深度检测
-  viewer.scene.globe.depthTestAgainstTerrain = true;
-
-
-  // // Cesium处理iframe的allow-scripts权限问题
+  viewer.value.scene.globe.depthTestAgainstTerrain = true;
+  emit('cesium', viewer.value)
+  // // Cesium处理iframe的allow-scripts权限问题(infoBox: false的另外一种方法)
   // const iframe = document.getElementsByClassName("cesium-infoBox-iframe")[0]; // 获取iframe dom元素
   // iframe.setAttribute(
   //   "sandbox",
   //   "allow-same-origin allow-scripts allow-popups allow-forms"
   // );
   // iframe.setAttribute("src", "");
-
 });
+onBeforeUnmount(() => {
+  viewer.value = null
+})
 </script> 
 <style lang="less" scoped>
 #cesiumContainer {
